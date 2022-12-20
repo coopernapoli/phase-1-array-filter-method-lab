42e64f7959e26bc7a670a09979f591607975a03d
Sean <napolisc10@gmail.com>
2022-12-20 16:57:00 -0700

    commit tree b80ce5c6565b4d9e33670c214e0fa1925ce03f2c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter( 
+        driver => driver.toLowerCase()=== names.toLowerCase())
+}
+
+function fuzzyMatch(drivers, names) {
+    console.log(names)
+    return drivers.filter(
+        driver => {
+            const firstLetterNames =  names.toLowerCase();
+           
+            const firstLetterDriver = driver.toLowerCase();
+
+            return firstLetterDriver.startsWith(firstLetterNames)
+        }
+    )
+}
+
+
+// const driverObjectArray =[ {
+//     name: "Marin",
+//     hometown: "Poway",
+// }]
+
+
+function matchName (driverObjectArray, name) {
+    return driverObjectArray.filter(driver => name===driver.name)
+}
